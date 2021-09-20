c6b4c0b2050eb2deffc09799b193742f9b9b4a6e
Juliana Gomes <juliana.silvagomes93@gmail.com>
2021-09-20 14:31:58 -0300

    Criando os componentes de Restaurantes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from "@angular/router";
 import { AboutComponent } from "./about/about.component";
 import { HomeComponent } from "./home/home.component";
+import { RestaurantsComponent } from "./restaurants/restaurants.component";
 
 
 //Arquivo de rotas da nossa aplicação, utilizamos a tipagem Routes do Angular
 export const ROUTES: Routes = [
     {path: '', component: HomeComponent},
+    {path: 'restaurants', component: RestaurantsComponent},  //Mostrando o conteúdo da página restaurants
     {path: 'about', component: AboutComponent}  //Mostrando o conteúdo da página about
 ];
