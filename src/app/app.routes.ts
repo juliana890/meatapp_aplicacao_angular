6e4aeda0991c3ddb765965ef26a96a3d17839e46
Juliana Gomes <juliana.silvagomes93@gmail.com>
2021-09-28 14:43:11 -0300

    Rotas filhas para Avaliações e Menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from "@angular/router";
 import { AboutComponent } from "./about/about.component";
 import { HomeComponent } from "./home/home.component";
+import { MenuComponent } from "./restaurant-detail/menu/menu.component";
 import { RestaurantDetailComponent } from "./restaurant-detail/restaurant-detail.component";
+import { ReviewsComponent } from "./restaurant-detail/reviews/reviews.component";
 import { RestaurantsComponent } from "./restaurants/restaurants.component";
 
 
@@ -9,6 +11,12 @@ import { RestaurantsComponent } from "./restaurants/restaurants.component";
 export const ROUTES: Routes = [
     {path: '', component: HomeComponent},
     {path: 'restaurants', component: RestaurantsComponent},  //Mostrando o conteúdo da página restaurants
-    {path: 'restaurants/:id', component: RestaurantDetailComponent}, //':' indica parâmetro na rota
+    {path: 'restaurants/:id', component: RestaurantDetailComponent,
+        children:[
+            {path: '', redirectTo: 'menu', pathMatch: 'full'},
+            {path: 'menu', component: MenuComponent},
+            {path: 'reviews', component: ReviewsComponent}
+        ]
+    }, //':' indica parâmetro na rota
     {path: 'about', component: AboutComponent}  //Mostrando o conteúdo da página about
 ];
